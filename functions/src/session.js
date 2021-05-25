52fecc92b092bc81e32785ffbeb1a04285c73bc8
jreynaud2 <jean.reynaud@ecal.ch>
2021-05-25 13:38:29 +0200

    allow

--- functions/src/session.js
+++ functions/src/session.js
@@ -25,7 +25,10 @@ const createSession = (name) => {
       })
     })
   }
-
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type'
+  }
   exports.handler = async (event, context) => {
     if (event.httpMethod == 'OPTIONS') {
         return { 
